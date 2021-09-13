11a43dbc5c582462efd94dce0d157c3653238078
CarlosSampedro <csampedro@makingsense.com>
2021-09-13 20:23:59 -0300

    [Task4_AddClientAndRentalObjects] I also have
     added some delayed features as unique interfaces, generics and
    taked off writeline commando from the methods. Next Pr's will
    be smallers :)

--- ClientCRUD.cs
+++ ClientCRUD.cs
@@ -3,68 +3,113 @@ using ProyectoAgencia;
 using ProyectoAgencia.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 
 namespace ProjectAgency
 {
-    public class ClientCRUD : IClientCRUD
+    public class ClientCRUD : ICRUD<Client>
     {
-        List<Client> allClients = new List<Client>();
-        private string clientJsonFile = ConfigurationManager.AppSettings[("pathClients")];
-        string clientJsonString = File.ReadAllText("client.json");//I didn't use an "using" block because File.ReadAllText close the file at the end.
-        Client client = new Client();
-
-
-        public Client Get(string Id)
+        private readonly string _clientFilePath;//private variables starts with _
+        public ClientCRUD(string clientFilePath)
         {
-            allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
+            _clientFilePath = clientFilePath;
+        }
+
+        public Client Get(int Id)
+        {
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
 
             foreach (Client client in allClients)
             {
-                if (Convert.ToString(Id).Equals(client.DNI))
+                if (Convert.ToInt32(Id).Equals(client.Id))
                 {
-                    Console.WriteLine($"DNI: {client.DNI}");
-                    Console.WriteLine($"Name: {client.Name}");
-                    Console.WriteLine($"Lastname: {client.Lastname}");
-                    Console.WriteLine($"Phone: {client.Phone}");
-                    Console.WriteLine($"Address: {client.Address}");
-                    Console.WriteLine($"City: {client.City}");
-                    Console.WriteLine($"Cp: {client.Cp}");
-                    Console.WriteLine($"LastUpdatedDate: {client.LastUpdatedDate}");
                     return client;
                 }
             }
-            return client;
+            return null;
         }
+        
+        public bool CheckUniqueDni(string dni)
+        {
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
 
+            foreach (Client element in allClients)
+            {
+                if (Convert.ToInt32((dni)).Equals(element.DNI))
+                {
+                    string clientJson = JsonConvert.SerializeObject(allClients);
+                    return false;
+                }                
+            }
+            return true;
+        }
 
         public Client Create(Client client)
         {
-            allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
-            Console.WriteLine("Enter the DNI:");
-            client.DNI = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Name:");
-            client.Name = Console.ReadLine();
-            Console.WriteLine("Enter the Lastname:");
-            client.Lastname = Console.ReadLine();
-            Console.WriteLine("Enter the Phone:");
-            client.Phone = Console.ReadLine();
-            Console.WriteLine("Enter the Address");
-            client.Address = Console.ReadLine();
-            Console.WriteLine("Enter the City:");
-            client.City = Console.ReadLine();
-            Console.WriteLine("Is the Cp:");
-            client.Cp = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Is the LastUpdatedDate:");
-            client.LastUpdatedDate = Console.ReadLine();
-
-            allClients.Add(client);
-            Console.WriteLine("Client was added succesfully !!");
-            string clientJson = JsonConvert.SerializeObject(allClients);
-            File.WriteAllText(clientJsonFile, clientJson);
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
+            
+            bool IsDniUnique = CheckUniqueDni(Convert.ToString(client.DNI));
+            if (IsDniUnique == true) 
+            {
+                allClients.Add(client);
+                Console.WriteLine("Client was added succesfully !!");
+                File.WriteAllText(_clientFilePath, JsonConvert.SerializeObject(allClients));
+                return client;
+            }
+            Console.WriteLine("Client was NOT added becasue DNI is duplicated !!");
             return client;
         }
 
+        public void Delete(string Id)
+        {
+
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
+
+            foreach (Client element in allClients)
+            {
+                if (Convert.ToInt32(Id).Equals(element.Id))
+                {
+                    allClients.Remove(element);
+                    string clientJson = JsonConvert.SerializeObject(allClients);
+                    File.WriteAllText(_clientFilePath, clientJson);
+                    break;
+                }
+            }
+        }
+
+        public void Update(Client client)
+        {
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
+
+            for (var i = 0; i < allClients.Count; i++)
+            {
+                if (client.Id == (allClients[i].Id))
+                {
+                    allClients[i].DNI = client.DNI;
+                    allClients[i].Name = client.Name;
+                    allClients[i].Lastname = client.Lastname;
+                    allClients[i].Phone = client.Phone;
+                    allClients[i].Address = client.Address;
+                    allClients[i].City = client.City;
+                    allClients[i].Cp = client.Cp;
+                    allClients[i].LastUpdatedDate = client.LastUpdatedDate;
+                    string json = JsonConvert.SerializeObject(allClients);
+                    File.WriteAllText(_clientFilePath, json);
+                }
+            }
+        }
+
+        public List<Client> ListAll()
+        {
+            var clientJsonString = File.ReadAllText(_clientFilePath);
+            var allClients = JsonConvert.DeserializeObject<List<Client>>(clientJsonString);
+
+            return allClients;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
 ﻿using ProyectoAgencia;
 using System;
+using System.Configuration;
 
 namespace ProjectAgency
 {
     public class Program
     {
-        public CarCRUD carCrud;
-        public Car car;
-        public ClientCRUD clientCrud;
-        public Client client;
-
-        static void Main(string[] args)
+        static void Main()
         {                        
             bool leave = false;
 
-
             while (!leave)
             {
-                Car car = new Car();
-                CarCRUD carCrud = new CarCRUD();
-                Client client = new Client();
-                ClientCRUD clientCrud = new ClientCRUD();
+                var car = new Car();
+                var carCrud = new CarCRUD(ConfigurationManager.AppSettings[("pathCars")]);
+                var client = new Client();
+                var clientCrud = new ClientCRUD(ConfigurationManager.AppSettings[("pathClients")]);
+                var rental = new Rental();
+                var rentalCrud = new RentalCRUD(ConfigurationManager.AppSettings[("pathRentals")]);
+
                 Console.WriteLine("-----------------");
                 Console.WriteLine("CAR MENU");
                 Console.WriteLine("-----------------");
@@ -29,14 +27,27 @@ namespace ProjectAgency
                 Console.WriteLine("3. Delete car option");
                 Console.WriteLine("4. Update car option");
                 Console.WriteLine("5. List All Cars");
-                Console.WriteLine("6. Leave");
                 Console.WriteLine("");
                 Console.WriteLine("-----------------");
-                Console.WriteLine("CLIENT MENU //WIP");
+                Console.WriteLine("CLIENT MENU");
                 Console.WriteLine("-----------------");
-                Console.WriteLine("7. Get client information option //WIP");
+                Console.WriteLine("6. Get client information option");
+                Console.WriteLine("7. Add client option");
+                Console.WriteLine("8. Delete client optionP");
+                Console.WriteLine("9. Update client option");
+                Console.WriteLine("10. List All Client");
                 Console.WriteLine("");
-
+                Console.WriteLine("-----------------");
+                Console.WriteLine("RENTAL MENU");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("11. Get rental information option");
+                Console.WriteLine("12. Add rental option");
+                Console.WriteLine("13. Delete rental option");
+                Console.WriteLine("14. Update rental option");
+                Console.WriteLine("15. List All rentals");
+                Console.WriteLine("");
+                Console.WriteLine("16. Leave");
+                Console.WriteLine("");
                 Console.WriteLine("Select an option");
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -45,12 +56,33 @@ namespace ProjectAgency
                     case 1:
                         Console.WriteLine("You have chosen Get car information option");
                         Console.WriteLine("Enter the car Id");
-                        var Id = Console.ReadLine();
-                        carCrud.Get(Id);
+                        car = carCrud.Get(Convert.ToInt32(Console.ReadLine()));
+                        Console.WriteLine($"Patent: {car.Id}");
+                        Console.WriteLine($"Patent: {car.Patent}");
+                        Console.WriteLine($"Brand: {car.Brand}");
+                        Console.WriteLine($"Model: {car.Model}");
+                        Console.WriteLine($"DoorsAmount: {car.DoorsAmount}");
+                        Console.WriteLine($"Color: {car.Color}");
+                        Console.WriteLine($"Automatic: {car.Automatic}");
                         break;
 
                     case 2:
                         Console.WriteLine("You have chosen Add car option");
+                        Console.WriteLine("Enter the Id:");
+                        car.Id = Console.ReadLine();
+                        Console.WriteLine("Enter the patent:");
+                        car.Patent = Console.ReadLine();
+                        Console.WriteLine("Enter the brand:");
+                        car.Brand = Console.ReadLine();
+                        Console.WriteLine("Enter the model:");
+                        car.Model = Console.ReadLine();
+                        Console.WriteLine("Enter the doors amount:");
+                        car.DoorsAmount = Convert.ToByte(Console.ReadLine());
+                        Console.WriteLine("Enter the color:");
+                        car.Color = Console.ReadLine();
+                        Console.WriteLine("Is the car automatic?(true/false):");
+                        car.Automatic = Convert.ToBoolean(Console.ReadLine());
+                        Console.WriteLine("Car was added succesfully !!");
                         carCrud.Create(car);
                         break;
 
@@ -59,30 +91,219 @@ namespace ProjectAgency
                         Console.WriteLine("Enter the car Id to be deleted");
                         var patentToDelete = Console.ReadLine();
                         carCrud.Delete(patentToDelete);
+                        Console.WriteLine($"Id: {patentToDelete} deleted !!");
                         break;
 
-                    case 4:   
+                    case 4:
                         Console.WriteLine("You have chosen Update car option");
                         Console.WriteLine("Enter the car Id to be updated");
-                        var patentToUpdate = Console.ReadLine();
-                        carCrud.Update(patentToUpdate);
+                        car.Id = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW patent:");
+                        car.Patent = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW brand:");
+                        car.Brand = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW model:");
+                        car.Model = Console.ReadLine();
+                        Console.WriteLine("Enter the doors NEW amount:");
+                        car.DoorsAmount = Convert.ToByte(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW color:");
+                        car.Color = Console.ReadLine();
+                        Console.WriteLine("UPDATE - Is the car automatic?(true/false):");
+                        car.Automatic = Convert.ToBoolean(Console.ReadLine());
+                        Console.WriteLine("Car was Updated succesfully !!");
+                        carCrud.Update(car);
                         break;
 
                     case 5:
                         Console.WriteLine("You have chosen List All Cars option");
-                        carCrud.ListAll();
+                        var allCars = carCrud.ListAll();
+
+                        for (var i = 0; i < allCars.Count; i++)
+                        {
+                            car.Id = allCars[i].Id;
+                            Console.WriteLine($"Car Id: {car.Id}");
+                            car.Patent = allCars[i].Patent;
+                            Console.WriteLine($"Patent: {car.Patent}");
+                            car.Brand = allCars[i].Brand;
+                            Console.WriteLine($"Brand: {car.Brand}");
+                            car.Model = allCars[i].Model;
+                            Console.WriteLine($"Model: {car.Model}");
+                            car.DoorsAmount = allCars[i].DoorsAmount;
+                            Console.WriteLine($"DoorsAmount: {car.DoorsAmount}");
+                            car.Color = allCars[i].Color;
+                            Console.WriteLine($"Color: {car.Color}");
+                            car.Automatic = allCars[i].Automatic;
+                            Console.WriteLine($"Automatic: {car.Automatic}");
+                        }
                         break;
 
                     case 6:
-                        Console.WriteLine("You have chosen Leave option");
-                        leave = true;
+                        Console.WriteLine("You have chosen Get client information option");
+                        Console.WriteLine("Enter the client Id");
+                        client = clientCrud.Get(Convert.ToInt32(Console.ReadLine()));
+                        Console.WriteLine($"Id: {client.Id}");
+                        Console.WriteLine($"DNI: {client.DNI}");
+                        Console.WriteLine($"Name: {client.Name}");
+                        Console.WriteLine($"Lastname: {client.Lastname}");
+                        Console.WriteLine($"Phone: {client.Phone}");
+                        Console.WriteLine($"Address: {client.Address}");
+                        Console.WriteLine($"City: {client.City}");
+                        Console.WriteLine($"Cp: {client.Cp}");
+                        Console.WriteLine($"LastUpdatedDate: {client.LastUpdatedDate}");
                         break;
 
                     case 7:
                         Console.WriteLine("You have chosen Create client option");
+                        Console.WriteLine("Enter the Id:");
+                        client.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the DNI:");
+                        client.DNI = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Name:");
+                        client.Name = Console.ReadLine();
+                        Console.WriteLine("Enter the Lastname:");
+                        client.Lastname = Console.ReadLine();
+                        Console.WriteLine("Enter the Phone:");
+                        client.Phone = Console.ReadLine();
+                        Console.WriteLine("Enter the Address");
+                        client.Address = Console.ReadLine();
+                        Console.WriteLine("Enter the City:");
+                        client.City = Console.ReadLine();
+                        Console.WriteLine("Is the Cp:");
+                        client.Cp = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Is the LastUpdatedDate:");
+                        client.LastUpdatedDate = Console.ReadLine();
                         clientCrud.Create(client);
                         break;
 
+                    case 8:   
+                        Console.WriteLine("You have chosen Delete client option");
+                        Console.WriteLine("Enter the client Id to be deleted");
+                        var clientToBeDelete = (Console.ReadLine());                        
+                        clientCrud.Delete(clientToBeDelete);
+                        Console.WriteLine($"Idt: {clientToBeDelete} deleted !!");
+                        break;
+
+                    case 9:
+                        Console.WriteLine("You have chosen Update client option");
+                        Console.WriteLine("Enter the client Id to be updated");
+                        client.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW DNI:");
+                        client.DNI = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW Name:");
+                        client.Name = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW Lastname:");
+                        client.Lastname = Console.ReadLine();
+                        Console.WriteLine("Enter the doors NEW phone:");
+                        client.Phone = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW address:");
+                        client.Address = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW City:");
+                        client.City = Console.ReadLine();
+                        Console.WriteLine("Enter the NEW Cp");
+                        client.Cp = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW LastUpdatedDate");
+                        client.LastUpdatedDate = Console.ReadLine();
+                        Console.WriteLine("Client was Updated succesfully !!");
+                        clientCrud.Update(client);
+                        break;
+
+                    case 10:
+                        Console.WriteLine("You have chosen List All Clients option");
+                        var allClient = clientCrud.ListAll();
+                        for (var i = 0; i < allClient.Count; i++)
+                        {
+                            client.Id = allClient[i].Id;
+                            Console.WriteLine($"Client Id: {client.Id}");
+                            client.DNI = allClient[i].DNI;
+                            Console.WriteLine($"DNI: {client.DNI}");
+                            client.Name = allClient[i].Name;
+                            Console.WriteLine($"Name: {client.Name}");
+                            client.Lastname = allClient[i].Lastname;
+                            Console.WriteLine($"Lastname: {client.Lastname}");
+                            client.Phone = allClient[i].Phone;
+                            Console.WriteLine($"Phone: {client.Phone}");
+                            client.Address = allClient[i].Address;
+                            Console.WriteLine($"Address: {client.Address}");
+                            client.City = allClient[i].City;
+                            Console.WriteLine($"City: {client.City}");
+                            client.Cp = allClient[i].Cp;
+                            Console.WriteLine($"Cp: {client.Cp}");
+                            client.LastUpdatedDate = allClient[i].LastUpdatedDate;
+                            Console.WriteLine($"LastUpdatedDate: {client.LastUpdatedDate}");
+                        }
+                        break;
+
+                    case 11:
+                        Console.WriteLine("You have chosen Get rental information option");
+                        Console.WriteLine("Enter the rental Id");
+                        rental = rentalCrud.Get(Convert.ToInt32(Console.ReadLine()));
+                        Console.WriteLine($"Id: {rental.Id}");
+                        Console.WriteLine($"RentalDuration: {rental.RentalDuration}");
+                        Console.WriteLine($"RentalClientId: {rental.RentalClientId}");
+                        Console.WriteLine($"RentalCarId: {rental.RentalCarId}");
+                        Console.WriteLine($"RentalReturnDate: {rental.RentalReturnDate}");
+                        break;
+
+                    case 12:
+                        Console.WriteLine("You have chosen Create rental option");
+                        Console.WriteLine("Enter the Id:");
+                        rental.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Rental duration:");
+                        rental.RentalDuration = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Client Id:");
+                        rental.RentalClientId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Rental Car Id:");
+                        rental.RentalCarId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Rental Returned Date:");
+                        rental.RentalReturnDate = Console.ReadLine();
+                        rentalCrud.Create(rental);
+                        Console.WriteLine("Rental was added succesfully !!");
+                        break;
+
+                    case 13:
+                        Console.WriteLine("You have chosen Delete rental option");
+                        Console.WriteLine("Enter the rental Id to be deleted");
+                        rentalCrud.Delete(Console.ReadLine());
+                        break;
+
+                    case 14:
+                        Console.WriteLine("You have chosen Update rental option");
+                        Console.WriteLine("Enter the rental Id to be updated");
+                        rental.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW Rental Duration:");
+                        rental.RentalDuration = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW Rental Client Id:");
+                        rental.RentalClientId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the NEW Rental Car Id:");
+                        rental.RentalCarId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the doors NEW Rental Return Date:");
+                        rental.RentalReturnDate = Console.ReadLine();
+                        Console.WriteLine("Rental was Updated succesfully !!");
+                        rentalCrud.Update(rental);
+                        break;
+
+                    case 15:
+                        Console.WriteLine("You have chosen List All rentals option");
+                        var allRentals = rentalCrud.ListAll();
+                        for (var i = 0; i < allRentals.Count; i++)
+                        {
+                                rental.Id = allRentals[i].Id;
+                                Console.WriteLine($"RentalId: {rental.Id}");
+                                rental.RentalDuration = allRentals[i].RentalDuration;
+                                Console.WriteLine($"RentalDuration: {rental.RentalDuration}");
+                                rental.RentalClientId = allRentals[i].RentalClientId;
+                                Console.WriteLine($"RentalClientId: {rental.RentalClientId}");
+                                rental.RentalCarId = allRentals[i].RentalCarId;
+                                Console.WriteLine($"RentalCarId: {rental.RentalCarId}");
+                                rental.RentalReturnDate = allRentals[i].RentalReturnDate;
+                                Console.WriteLine($"RentalReturnDate: {rental.RentalReturnDate}");                            
+                        }
+                        break;
+
+                    case 16:
+                        Console.WriteLine("You have chosen Leave option");
+                        leave = true;
+                        break;
 
                     default:
                         Console.WriteLine("Select an option");
